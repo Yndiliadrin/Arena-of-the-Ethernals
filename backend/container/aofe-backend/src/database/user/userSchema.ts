7fd8226022f151be375a7b6063c473d051c527f6
Yndiliädrin <baloghetele285@gmail.com>
2023-05-01 10:26:09 +0200

    regiszt works

--- backend/container/aofe-backend/src/database/user/userSchema.ts
+++ backend/container/aofe-backend/src/database/user/userSchema.ts
@@ -1,43 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
-import { itemSchema } from "../item/itemSchema.js";
-
 const characterSchema = new Schema({
   strenght: {
     type: Number,
     required: true,
-    default: 1
+    default: 1,
   },
   dexterity: {
     type: Number,
     required: true,
-    default: 1
+    default: 1,
   },
   intelligence: {
     type: Number,
     required: true,
-    default: 1
+    default: 1,
   },
   equipment: {
-    type: [itemSchema],
+    type: [Schema.Types.ObjectId],
     required: true,
+    ref: "Item",
     default: [],
   },
   level: {
     type: Number,
     required: true,
-    default: 0
-  }
+    default: 0,
+  },
 });
 
-const Character = mongoose.model("character", characterSchema)
+export const Character = mongoose.model("npc", characterSchema);
 
 export const userSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
@@ -53,47 +52,48 @@ export const userSchema = new Schema({
   },
   character: {
     type: characterSchema,
-    default: new Character()
+    default: new Character(),
   },
 });
 
-
 // #3 A user sémájához egy pre-hookot adunk hozzá, amely a mentés előtt fut le
-userSchema.pre('save', function(next) {
+userSchema.pre("save", function (next) {
   const user = this;
   // Ellenőrizzük, hogy a jelszó módosult-e
-  if(user.isModified('password')) {
-      // Generálunk egy sót a jelszó hash-eléséhez
-      bcrypt.genSalt(10, function(err, salt) {
-          if(err) {
-              console.log('hiba a salt generalasa soran');
-              // Ha hiba történik a só generálásakor, akkor visszatérünk a hibával
-              return next(err);
-          }
-          // Hash-eljük a jelszót a sóval
-          bcrypt.hash(user.password, salt, function(error, hash) {
-              if(error) {
-                  console.log('hiba a hasheles soran');
-                  // Ha hiba történik a hash-elés során, akkor visszatérünk a hibával
-                  return next(error);
-              }
-              // Beállítjuk a jelszó értékét a hash-re
-              user.password = hash;
-              user.salt = salt;
-              return next();
-          })
-      })
+  if (user.isModified("password")) {
+    // Generálunk egy sót a jelszó hash-eléséhez
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        console.log("hiba a salt generalasa soran");
+        // Ha hiba történik a só generálásakor, akkor visszatérünk a hibával
+        return next(err);
+      }
+      // Hash-eljük a jelszót a sóval
+      bcrypt.hash(user.password, salt, function (error, hash) {
+        if (error) {
+          console.log("hiba a hasheles soran");
+          // Ha hiba történik a hash-elés során, akkor visszatérünk a hibával
+          return next(error);
+        }
+        // Beállítjuk a jelszó értékét a hash-re
+        user.password = hash;
+        user.salt = salt;
+        return next();
+      });
+    });
   } else {
-      // Ha a jelszó nem módosult, akkor folytatjuk a mentést
-      return next();
+    // Ha a jelszó nem módosult, akkor folytatjuk a mentést
+    return next();
   }
 });
 
-userSchema.methods.comparePasswords = function(password: string, nx: Function) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-      nx(err, isMatch);
+userSchema.methods.comparePasswords = function (
+  password: string,
+  nx: Function
+) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    nx(err, isMatch);
   });
 };
 
-
-export const User = mongoose.model("user", userSchema)
+export const User = mongoose.model("user", userSchema);
