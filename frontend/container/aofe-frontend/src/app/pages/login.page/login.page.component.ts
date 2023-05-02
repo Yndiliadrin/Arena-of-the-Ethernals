73546965d08343075bcc976cb2df5983f5c2d51f
Balogh Etele <baloghe.etele@inf.u-szeged.hu>
2023-05-02 13:54:24 +0200

    Login page in progress

--- frontend/container/aofe-frontend/src/app/pages/login.page/login.page.component.ts
+++ frontend/container/aofe-frontend/src/app/pages/login.page/login.page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login.page',
   templateUrl: './login.page.component.html',
-  styleUrls: ['./login.page.component.scss']
+  styleUrls: ['./login.page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
+  hide = true;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (localStorage.getItem('user')) {
+      localStorage.removeItem('user');
+      this.authService.logout().subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => console.log
+      );
+    }
+  }
+
+  onSubmit(): void {
+    this.authService.login(this.form.value).subscribe(
+      (res) => {
+        console.log(res);
+        localStorage.setItem('user', this.form.get('username')?.value);
+      },
+      (err) => console.log(err)
+    );
+  }
 }
--- frontend/container/aofe-frontend/src/app/pages/login.page/login.page.module.ts
+++ frontend/container/aofe-frontend/src/app/pages/login.page/login.page.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login.page.component';
-
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    LoginPageComponent
-  ],
+  declarations: [LoginPageComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
+  ],
 })
-export class LoginPageModule { }
+export class LoginPageModule {}
