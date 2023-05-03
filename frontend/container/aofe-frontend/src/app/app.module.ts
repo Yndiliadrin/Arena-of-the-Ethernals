43fe589e09f89bb7df70058181309d0732b422fe
Balogh Etele <baloghe.etele@inf.u-szeged.hu>
2023-05-03 10:45:22 +0200

    minor refactor 2.0

--- frontend/container/aofe-frontend/src/app/app.module.ts
+++ frontend/container/aofe-frontend/src/app/app.module.ts
@@ -5,21 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatButtonModule} from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatCardModule } from '@angular/material/card';
 import { LoginPageModule } from './pages/login.page/login.page.module';
 import { RegisztPageModule } from './pages/regiszt.page/regiszt.page.module';
 import { IndexPageModule } from './pages/index.page/index.page.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,9 +20,9 @@ import { IndexPageModule } from './pages/index.page/index.page.module';
 
     LoginPageModule,
     RegisztPageModule,
-    IndexPageModule
+    IndexPageModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
