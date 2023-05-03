edb7f3fd2e24e726bfce071c8e386194fc85842d
Balogh Etele <baloghe.etele@inf.u-szeged.hu>
2023-05-03 10:44:38 +0200

    Sidenav added

--- frontend/container/aofe-frontend/src/app/pages/index.page/index.page.component.ts
+++ frontend/container/aofe-frontend/src/app/pages/index.page/index.page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {  Router } from '@angular/router';
+import { SettingsComponent } from 'src/app/components/settings/settings.component';
 
 @Component({
   selector: 'app-index.page',
   templateUrl: './index.page.component.html',
-  styleUrls: ['./index.page.component.scss']
+  styleUrls: ['./index.page.component.scss'],
 })
-export class IndexPageComponent {
+export class IndexPageComponent implements OnInit {
+  constructor(public dialog: MatDialog, private router: Router) {}
 
+  ngOnInit(): void {
+
+  }
+
+  openSettingsDialog(): void {
+    const dialogRef = this.dialog.open(SettingsComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  logout(): void {
+    this.router.navigate(["/login"])
+  }
 }
--- frontend/container/aofe-frontend/src/app/pages/index.page/index.page.module.ts
+++ frontend/container/aofe-frontend/src/app/pages/index.page/index.page.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexPageComponent } from './index.page.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SettingsModule } from 'src/app/components/settings/settings.module';
 @NgModule({
   declarations: [IndexPageComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+
+    SettingsModule
+  ],
 })
 export class IndexPageModule {}
