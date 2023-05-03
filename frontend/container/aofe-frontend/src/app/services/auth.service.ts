31247706f55e0585ebf3b94ccbf3b39760c7a9af
Balogh Etele <baloghe.etele@inf.u-szeged.hu>
2023-05-03 10:45:08 +0200

    Minor refactors

--- frontend/container/aofe-frontend/src/app/services/auth.service.ts
+++ frontend/container/aofe-frontend/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../shared/types/user.type';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ export class AuthService {
   constructor(private router: Router, private http: HttpClient) {}
 
   login(loginData: { username: string; password: string }) {
-    return this.http.post(`${this.rootUrl}/login`, loginData);
+    return this.http.post<User>(`${this.rootUrl}/login`, loginData);
   }
 
   logout() {
