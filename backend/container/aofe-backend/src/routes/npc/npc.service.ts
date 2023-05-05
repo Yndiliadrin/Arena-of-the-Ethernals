81fb67ff06a0d095ace62e1e11a44f73a68f1937
Balogh Etele <baloghe.etele@inf.u-szeged.hu>
2023-05-05 14:31:04 +0200

    Updated npc API

--- backend/container/aofe-backend/src/routes/npc/npc.service.ts
+++ backend/container/aofe-backend/src/routes/npc/npc.service.ts
@@ -1,3 +1,61 @@
+import { Npc } from "../../database/npc/npcSchema.js";
+
 export const getNpcs = (req, res) => {
   res.json(res.npc);
+};
+
+/**
+ * Creates new NPC in the database
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export const createNpc = async (req, res) => {
+  try {
+    const npc = new Npc(req.body);
+    await npc.save();
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
+  } finally {
+    return res.status(201).json({ success: true });
+  }
+};
+
+/**
+ * Updates the NPC int the database
+ * 
+ * @param req
+ * @param res
+ * @returns
+ */
+export const updateNpc = async (req, res) => {
+  try {
+    const npc = new Npc(req.body);
+    await npc.save();
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
+  } finally {
+    return res.status(201).json({ success: true });
+  }
+};
+
+/**
+ * Delete NPC record from the database (req.params.id)
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export const deleteNpc = async (req, res) => {
+  try {
+    await Npc.deleteOne({ _id: req.params.id });
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
+  } finally {
+    return res.status(200).json({ success: true });
+  }
 };
